74cfe7bb4ab4ad6b6ea43eb6b31707fc69d54f1e
Paul Tagliamonte <tag@pault.ag>
2013-10-03 19:17:56 -0400

    foo

--- debile/master/server.py
+++ debile/master/server.py
@@ -19,8 +19,6 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-#from debile.master.orm import 
-
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
 
