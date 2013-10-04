c72914ea5beb6f4378e049e30f0491846f86d65a
Paul Tagliamonte <tag@pault.ag>
2013-10-03 21:43:10 -0400

    foo

--- debile/utils/dud.py
+++ debile/utils/dud.py
@@ -1,4 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from debian import deb822
+from debile.utils import run_command
 
 
 class Dud(object):
-    pass
+    def __init__(self, filename):
+        pass
+
+    def add_file(self, fp):
+        pass
+
+    def validate_signature(self):
+        pass
+
+    def validate_hashes(self):
+        pass
+
+    def validate(self):
+        pass
+
+    def get_files(self):
+        pass
