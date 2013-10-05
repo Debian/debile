121355b939b7c24e142960c0eaea9d77faf284ba
Paul Tagliamonte <tag@pault.ag>
2013-10-04 22:34:43 -0400

    Modify things. Make things happen.

--- setup.py
+++ setup.py
@@ -2,18 +2,43 @@ from debile import __appname__, __version__
 from setuptools import setup
 
 
+"""
+Dear world:
+
+     I'd like to say I'm sorry to anyone reading this. The amount of
+     scotch I had on hand wasn't enough to let me finish the debian
+     packaging hack I was working through (was working fine until I hit
+     some nonsense). As a result, this file will suffer.
+
+     Trust me, this is much better than what I had before.
+               -- PRT
+"""
+
+flavors = {
+    "setup.py": ([
+        'debile', 'debile.utils'
+    ], {}),  # Default config
+    "setup.slave.py": ([
+        'debile.slave'
+    ], {}),  # Slave config
+    "setup.master.py": (['debile.master'], {
+        'console_scripts': [
+            'debile-server-init = debile.master.cli:init',
+            'debile-incoming = debile.master.cli:process_incoming',
+            'debile-import = debile.master.cli:import_db',
+        ],
+    }),  # Master config
+}
+
+packages, scripts = flavors[__file__]
+
 long_description = ""
 
 setup(
     name=__appname__,
     version=__version__,
     scripts=[],
-    packages=[
-        'debile',
-        'debile.slave',
-        'debile.master',
-        'debile.utils',
-    ],
+    packages=packages,
     author="Paul Tagliamonte",
     author_email="tag@pault.ag",
     long_description=long_description,
@@ -21,11 +46,5 @@ setup(
     license="Expat",
     url="http://debile.debian.net/",
     platforms=['any'],
-    entry_points={
-        'console_scripts': [
-            'debile-server-init = debile.master.cli:init',
-            'debile-incoming = debile.master.cli:process_incoming',
-            'debile-import = debile.master.cli:import_db',
-        ],
-    }
+    entry_points=scripts,
 )
