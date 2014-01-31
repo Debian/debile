1f6ccff556913d40ec7540810973ec45d44c089d
Paul Tagliamonte <tag@pault.ag>
2014-01-30 21:59:12 -0500

    fix invocation

--- debile/master/interface.py
+++ debile/master/interface.py
@@ -80,7 +80,7 @@ class DebileMasterInterface(object):
     def close_job(self, job_id, failed):
         job = NAMESPACE.session.query(Job).get(job_id)
         job.finished_at = dt.datetime.utcnow()
-        job.close(failed)
+        job.close(NAMESPACE.session, failed)
 
         NAMESPACE.session.add(job)
         NAMESPACE.session.commit()
