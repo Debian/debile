6fb23117a48d3097f68ba0dbd9560debcd0b924a
Matthias Klumpp <matthias@tenstral.net>
2014-03-06 00:25:47 +0100

    Bash it!

--- contrib/tanglu/process-incoming.sh
+++ contrib/tanglu/process-incoming.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 # This is a helper script. It is only useful within the Tanglu
 # infrastructure. If you're not from Tanglu, just ignore it.
 source /var/archive-kit/debile/ENV/bin/activate
 cd /pub/ftp/incoming
-chmod 660 /pub/ftp/incoming/*
+[ "$(ls -A /pub/ftp/incoming)" ] && chmod 660 /pub/ftp/incoming/*
 debile-import .
 cd /tmp
--- contrib/tanglu/update-jobs.sh
+++ contrib/tanglu/update-jobs.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # This is a helper script. It is only useful within the Tanglu
 # infrastructure. If you're not from Tanglu, just ignore it.
 source /var/archive-kit/debile/ENV/bin/activate
