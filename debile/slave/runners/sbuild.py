Debian/debile

commit 462f3b8c5cfbbb5991c51d6d7a0c448bc75cc6ce

Allow jobs to insert additional files into the dud

debile/slave/runners/sbuild.py
@@ -150,7 +150,7 @@ def sbuild(package, maintainer, suite, affinity, build_arch, build_indep, analys
     base, _ = os.path.basename(package).rsplit(".", 1)
     changes = glob.glob("{base}_*.changes".format(base=base))
 
-    return (analysis, out, ftbfs, changes)
+    return (analysis, out, ftbfs, changes, None)
 
 
 def version():

debile/slave/commands/build.py
@@ -41,7 +41,7 @@ def run(dsc, package, job, firehose):
     else:
         changes = None
 
-    return (firehose, out, ftbfs, changes)
+    return (firehose, out, ftbfs, changes, None)
 
 
 def get_version():

debile/slave/commands/clang.py
@@ -41,7 +41,7 @@ def run(dsc, package, job, firehose):
     else:
         changes = None
 
-    return (firehose, out, ftbfs, changes)
+    return (firehose, out, ftbfs, changes, None)
 
 
 def get_version():

debile/slave/daemon.py
@@ -34,6 +34,8 @@ import sys
 import signal
 import logging
 import time
+import os.path
+import shutil
 
 
 shutdown_request = False
@@ -191,7 +193,7 @@ def run_job(config, job):
         run, version = load_module(job['check'])
         firehose = create_firehose(package, version)
 
-        firehose, log, failed, changes = run(
+        firehose, log, failed, changes, files = run(
             target, package, job, firehose)
 
         datestr = formatdate()
@@ -238,6 +240,11 @@ def run_job(config, job):
         with open('{prefix}.log'.format(prefix=prefix), 'wb') as fd:
             fd.write(log.encode('utf-8'))
         dud.add_file('{prefix}.log'.format(prefix=prefix))
+
+        if files is not None:
+            for f in files:
+                shutil.copyfile(f, os.path.basename(f))
+                dud.add_file(os.path.basename(f))
 
         dudf = "{prefix}.dud".format(prefix=prefix)
         with open(dudf, 'w') as fd:

debile/slave/runners/adequate.py
@@ -50,7 +50,7 @@ def adequate(chroot_name, target, package_name, analysis):
             failed = True
             analysis.results.append(issue)
 
-        return analysis, out, failed, None
+        return (analysis, out, failed, None, None)
 
 
 def version():

debile/slave/runners/coccinelle.py
@@ -60,7 +60,7 @@ def coccinelle(dsc, analysis):
             log += "DEAL patch %s\n" % semantic_patch
             log += "  %d results\n" % result_count
 
-    return (analysis, log, failed, None)
+    return (analysis, log, failed, None, None)
 
 
 def version():

debile/slave/runners/cppcheck.py
@@ -44,7 +44,7 @@ def cppcheck(dsc, analysis):
             ]:
                 failed = True
 
-        return (analysis, err, failed, None)
+        return (analysis, err, failed, None, None)
 
 
 def version():

debile/slave/runners/desktop_file_validate.py
@@ -37,7 +37,7 @@ def desktop_file_validate(package_root, analysis):
                 failed = True
             log += out
     log = log.strip()
-    return (analysis, log, failed, None)
+    return (analysis, log, failed, None, None)
 
 
 def version():

commit 0a0e0bd5d26cad8a7c2c3405fbd3b44a850f6ee0

462f3b8 broke sbuild. Fixed.

debile/slave/runners/sbuild.py
@@ -150,7 +150,7 @@ def sbuild(package, maintainer, suite, affinity, build_arch, build_indep, analys
     base, _ = os.path.basename(package).rsplit(".", 1)
     changes = glob.glob("{base}_*.changes".format(base=base))
 
-    return (analysis, out, ftbfs, changes, None)
+    return (analysis, out, ftbfs, changes)
 
 
 def version():